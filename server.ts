e839828cf90fe78e8a935f26b70f18f94333c89c
Michal Radomski <michalra@op.pl>
2022-08-15 13:23:37 +0200

    Server - initial commit

---
+++ server.ts
@@ -0,0 +1,17 @@
+const express = require("express");
+import {Request, Response} from "express";
+
+const app = express();
+
+const port = (process.env.PORT || 5000) as number;
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+  // For testing only
+  console.log("Current Time:", new Date().toLocaleTimeString());
+});
