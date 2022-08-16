974303143d4730e03a23e72508d1512e8499289f
Michal Radomski <michalra@op.pl>
2022-08-17 00:13:49 +0200

    Profile experience added

--- routes/Api/profile.ts
+++ routes/Api/profile.ts
@@ -180,3 +180,34 @@ router.delete("/", auth, async (req: CustomRequest, res: Response) => {
 });
 
 module.exports = router;
+
+// @route    PUT api/profile/experience
+// @desc     Add profile experience
+// @access   Private
+router.put(
+  "/experience",
+  auth,
+  check("title", "Title is required").notEmpty(),
+  check("company", "Company is required").notEmpty(),
+  check("from", "From date is required and needs to be from the past").notEmpty(),
+  async (req: CustomRequest, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+    const {title, company, location, form, to, current, description} = req.body;
+    const newExp = {title, company, location, form, to, current, description};
+    try {
+      const profile = await Profile.findOne({user: req.user.id});
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
