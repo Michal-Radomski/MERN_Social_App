5804d4dd1999d4539d0f020ed6f75a817cc472de
Michal Radomski <michalra@op.pl>
2022-08-24 01:10:15 +0200

    little updates

--- client/src/components/profile/ProfileGithub.tsx
+++ client/src/components/profile/ProfileGithub.tsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import {connect} from "react-redux";
 
 import {getGithubRepos} from "../../redux/actions/profile";
+import Spinner from "../layout/Spinner";
 
 const ProfileGithub = ({
   username,
@@ -12,7 +13,7 @@ const ProfileGithub = ({
   username: string;
   getGithubRepos: (arg0: string) => void;
   repos: Repo[];
-}) => {
+}): JSX.Element => {
   React.useEffect(() => {
     getGithubRepos(username);
   }, [getGithubRepos, username]);
@@ -20,25 +21,29 @@ const ProfileGithub = ({
   return (
     <div className="profile-github">
       <h2 className="text-primary my-1">Github Repos</h2>
-      {repos.map((repo: Repo) => (
-        <div key={repo.id} className="repo bg-white p-1 my-1">
-          <div>
-            <h4>
-              <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
-                {repo.name}
-              </a>
-            </h4>
-            <p>{repo.description}</p>
+      {repos === null ? (
+        <Spinner />
+      ) : (
+        repos.map((repo: Repo) => (
+          <div key={repo.id} className="repo bg-white p-1 my-1">
+            <div>
+              <h4>
+                <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                  {repo.name}
+                </a>
+              </h4>
+              <p>{repo.description}</p>
+            </div>
+            <div>
+              <ul>
+                <li className="badge badge-primary">Stars: {repo.stargazers_count}</li>
+                <li className="badge badge-dark">Watchers: {repo.watchers_count}</li>
+                <li className="badge badge-light">Forks: {repo.forks_count}</li>
+              </ul>
+            </div>
           </div>
-          <div>
-            <ul>
-              <li className="badge badge-primary">Stars: {repo.stargazers_count}</li>
-              <li className="badge badge-dark">Watchers: {repo.watchers_count}</li>
-              <li className="badge badge-light">Forks: {repo.forks_count}</li>
-            </ul>
-          </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
