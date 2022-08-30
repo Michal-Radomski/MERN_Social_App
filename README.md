62731c7ff61fb630553f023e77df158b936f3931
Michal Radomski <michalra@op.pl>
2022-08-31 00:06:46 +0200

    App done - last commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MERN_Social_App
 
 Files created during the Udemy.com course: <https://www.udemy.com/course/mern-stack-front-to-back>
+
+Test app: [https://mern-dev-connector-social-app.herokuapp.com](https://mern-dev-connector-social-app.herokuapp.com/).
--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 import {Request, Response} from "express";
 const connectDB = require("./config/db");
@@ -18,10 +19,21 @@ app.use("/api/posts", require("./routes/Api/posts"));
 
 const port = (process.env.PORT || 5000) as number;
 
-app.get("/", (req: Request, res: Response) => {
-  console.log("req.ip:", req.ip);
-  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
-});
+// app.get("/", (req: Request, res: Response) => {
+//   console.log("req.ip:", req.ip);
+//   res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+// });
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req: Request, res: Response) => {
+    console.log("req.ip:", req.ip);
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
